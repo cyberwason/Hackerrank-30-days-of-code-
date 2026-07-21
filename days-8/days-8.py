ce7830ee618d930ef6b960afb0ee7700adad07d1
cyberwason <matosrobert825@gmail.com>
2026-07-21 04:14:38 +0000

    Days-8 en hackerrank

---
+++ days-8/days-8.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    n = int(input())
+    phone_book = {}
+    
+    for _ in range(n):
+        name, number = input().split()
+        phone_book[name] = number
+    
+    while True:
+        try:
+            query = input()
+            if query in phone_book:
+                print(f"{query}={phone_book[query]}")
+            else:
+                print("Not found")
+        except EOFError:
+            break
